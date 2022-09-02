942d638908f9879b2521019a514fe5a19ed54d41
Ben Botsford <ben.botsford@flatironschool.com>
2022-09-02 09:25:59 -0500

    through routing views

---
+++ app/flask_app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    pass
